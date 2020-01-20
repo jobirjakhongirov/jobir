3764e1b4e54521221edba1a439c7301fea189749
Jobir Jakhongirov <jobirjakhongirov@gmail.com>
2020-01-20 21:11:40 +0900

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,53 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ Hamburger Button Animation
+~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+$(document).ready(function() {
+  $("#menu-button").click(function() {
+    $(this).toggleClass("close");
+    $("#desktop-navbar").toggleClass("desktop-nav-clicked");
+  });
+});
+
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ Smooth Page Scroll Animation
+~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+$(document).ready(function() {
+  $('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+      &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus(function() {
+            $(this).css("outline", "none");
+          });
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+});
